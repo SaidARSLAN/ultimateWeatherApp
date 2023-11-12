3dd83b1b753be2c43245809fcc2a7e97c3c05460
SaidARSLAN <lost.myreligion@hotmail.com>
2023-11-12 17:34:30 +0300

    Condition page fixed

--- src/contains/index.js
+++ src/contains/index.js
@@ -9,6 +9,12 @@ import explore from '../assets/explore.png'
 import location from '../assets/location.png'
 import settings from '../assets/settings.png'
 import heart from '../assets/Heart.png'
+import humidity from '../assets/humidity.png'
+import sun from '../assets/Sun.png'
+import vector from '../assets/Vector.png'
+import wind from '../assets/wind.png'
+import clock from '../assets/Clock.png'
+
 
 export const navLinks = [
     {icon:cloud,name:"weather"},
@@ -16,7 +22,15 @@ export const navLinks = [
     {icon:location,name:"cities"},
     {icon:settings,name:"settings"},
 ]
+export const airConditionLayouts = [
+    {icon:vector,name:"Real Feel"},
+    {icon:wind,name:"Wind"},
+    {icon:humidity,name:"Change of Rain"},
+    {icon:sun,name:"UV Index"},
+]
 
 
-
-export {backgroundImage,logo,explore,location,settings,cloud,heart}
+export {
+    backgroundImage,logo,explore,location,settings,cloud,heart,
+    humidity,sun,vector,wind,clock
+    }
